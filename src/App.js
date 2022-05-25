0b7ddbf30f3494c18b1248d796474fdbaa5b0de4
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-25 11:15:28 -0400

    wrote example promises

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 import './App.css';
 
 const promise = fetch("https://opentdb.com/api.php?amount=10")
-console.log(promise)
-promise.then((res) => console.log)
+console.log("first-promise", promise)
+promise.then((res) => {
+  console.log("first-results", res)
+  const nextPromise = res.json()
+  console.log("next-promise", nextPromise)
+  nextPromise.then((json) => console.log("json-results", json.results))
+})
+fetch("https://opentdb.com/api.php?amount=10").then((res) => res.json().then((json) => console.log("one-line-results", json.results)))
+
+
+const failedPromise = fetch("https://brokenurl.com")
+console.log("failed-promise", failedPromise)
+failedPromise.catch((err) => console.log("failed-promise-error", err))
+
+fetch("https://opentdb.com/api.php?amount=10")
+.then((res) => {
+  res.json()
+  .then((json) => {
+    console.log("properly-spaced-results", json.results)
+  })
+  .catch((err) => console.log(err))
+})
+.catch((err) => console.log(err))
+
+const asyncFunction = async () => {
+  const apiResults = await fetch("https://opentdb.com/api.php?amount=10")
+  const apiJSON = await apiResults.json()
+  console.log("async-results", apiJSON.results)
+  return apiJSON.results // add later
+}
+asyncFunction()
+
+const safeAsyncFunction = async () => {
+  try {
+    const apiResults = await fetch("https://brokenurl.com")
+    const apiJSON = await apiResults.json()
+    console.log("async-results", apiJSON.results)
+  } catch (err) {
+    console.log("async-error", err)
+  }
+}
+safeAsyncFunction()
+
+const myPromise = asyncFunction()
+console.log("my-promise", myPromise)
+myPromise.then((res) => console.log("myPromise-result", res[0]))
+
+/*
+Uses:
+1) Just do something... no need to worry about when it resolves
+  Write to database
+2) Read and update state... setState called at the end of the chain
+*/
+
+const promise1 = fetch("https://opentdb.com/api.php?amount=10")
+const promise2 = fetch("https://opentdb.com/api.php?amount=10")
+const promise3 = fetch("https://brokenurl.com")
+
+Promise.all([promise1, promise2, promise3])
+.then((allRes) => console.log("promise-chain-results", allRes))
+.catch((err) => console.log("promise-chain-error", err))
 
 
 
