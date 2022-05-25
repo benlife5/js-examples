347d36fa3d76019faa1925230b8f423e6d4bb8a3
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-25 10:49:33 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+const promise = fetch("https://opentdb.com/api.php?amount=10")
+console.log(promise)
+promise.then((res) => console.log)
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
